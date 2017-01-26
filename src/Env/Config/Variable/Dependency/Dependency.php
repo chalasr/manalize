c341172a26255c84eaac61e2e7b580e0e2b83af6
Robin Chalas <robin.chalas@gmail.com>
2017-01-26 09:51:27 +0100

    Tweak tests, CS

--- src/Env/Config/Variable/Dependency/Dependency.php
+++ src/Env/Config/Variable/Dependency/Dependency.php
@@ -23,10 +23,6 @@ class Dependency implements Variable
     protected $name;
     protected $enabled;
 
-    /**
-     * @param string $name
-     * @param bool   $enabled
-     */
     public function __construct(string $name, bool $enabled)
     {
         $this->name = $name;
